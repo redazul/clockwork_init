3bd929638c4ba7f422316be2cd3b7dc00141fb8e
azuldev <91290695+redazul@users.noreply.github.com>
2022-11-27 02:22:43 -0500

    Update clockwork_init.sh

--- clockwork_init.sh
+++ clockwork_init.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+echo "getting latest solana build"
+sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
+
+echo "removing old clockwork directory"
 rm -rf clockwork
-echo "configuring new clockwork worker"
+
+echo "getting rust"
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+rustup component add rustfmt
+
+sudo apt-get update
+sudo apt-get install libssl-dev libudev-dev pkg-config zlib1g-dev llvm clang cmake make libprotobuf-dev protobuf-compiler
+
+
+echo cloning clockwork 
 git clone https://github.com/clockwork-xyz/clockwork
+
+echo building target
 ./clockwork/scripts/build-all.sh clockwork/
 
