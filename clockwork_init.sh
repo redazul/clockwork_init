4ee53ba01d723ba8e10599e0f14dc9288e307de3
azuldev <91290695+redazul@users.noreply.github.com>
2022-11-27 02:13:47 -0500

    Update clockwork_init.sh

--- clockwork_init.sh
+++ clockwork_init.sh
@@ -2,5 +2,5 @@
 
 echo "configuring new clockwork worker"
 git clone https://github.com/clockwork-xyz/clockwork
-source ./clockwork/scripts/build-all.sh
+source ./clockwork/scripts/build-all.sh ./clockwork
 
