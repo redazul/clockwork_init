a7e8cdc0528228dbc6896851a3c20a81ec4708ac
azuldev <91290695+redazul@users.noreply.github.com>
2022-12-01 01:32:32 -0500

    Update validator.sh

--- validator.sh
+++ validator.sh
@@ -10,15 +10,15 @@ solana-validator \
 --geyser-plugin-config /root/clockwork/lib/geyser-plugin-config.json \
 --identity /root/validator-keypair.json \
 --rpc-port 8899 \
---entrypoint entrypoint.mainnet-beta.solana.com:8001 \
---entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
---entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
---entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
---entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
 --known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
 --known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
 --known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
 --known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
+--no-poh-speed-test \
+--no-port-check \
+--full-rpc-api \
+--only-known-rpc \
 --limit-ledger-size \
 --no-voting \
+--wal-recovery-mode \
 --log /var/log/solana/solana-validator.log &
