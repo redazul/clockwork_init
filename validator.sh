987628b37bca5725b534feca2692cc181177958d
azuldev <91290695+redazul@users.noreply.github.com>
2022-12-10 16:40:29 -0500

    Update validator.sh

--- validator.sh
+++ validator.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-pkill sol
+sudo pkill sol
 
 sudo $(command -v solana-sys-tuner) --user $(whoami) > sys-tuner.log 2>&1 &
 
 solana-validator \
---geyser-plugin-config /root/clockwork/lib/geyser-plugin-config.json \
---identity /root/validator-keypair.json \
---rpc-port 8899 \
---known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
---known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
---known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
---known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
---no-poh-speed-test \
---no-port-check \
---full-rpc-api \
---only-known-rpc \
---limit-ledger-size \
---no-voting \
---wal-recovery-mode skip_any_corrupted_record \
---log /var/log/solana/solana-validator.log &
+    --geyser-plugin-config /home/jc/clockwork/lib/geyser-plugin-config.json \
+    --identity /home/jc/.config/solana/id.json \
+    --known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
+    --known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
+    --known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
+    --known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
+    --only-known-rpc \
+    --ledger ledger \
+    --rpc-port 8899 \
+    --private-rpc \
+    --full-rpc-api \
+    --entrypoint entrypoint.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
+    --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
+    --wal-recovery-mode skip_any_corrupted_record \
+    --limit-ledger-size
+    --log /var/log/solana/solana-validator.log &
+
