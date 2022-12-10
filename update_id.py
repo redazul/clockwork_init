9678be6788c2d7257736f4df1fea34227705a820
azuldev <91290695+redazul@users.noreply.github.com>
2022-12-10 15:38:55 -0500

    Create update_id.py

---
+++ update_id.py
@@ -0,0 +1,12 @@
+import json
+
+
+print("updating worker")
+
+3with open("replayScript.json", "r") as jsonFile:
+#    data = json.load(jsonFile)
+
+#data["location"] = "NewPath"
+
+#with open("replayScript.json", "w") as jsonFile:
+#    json.dump(data, jsonFile)
