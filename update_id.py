316ba3071cfedc004877bb1453144fd40b720a31
azuldev <91290695+redazul@users.noreply.github.com>
2022-12-11 15:44:43 -0500

    Update update_id.py

--- update_id.py
+++ update_id.py
@@ -1,17 +1,14 @@
-import json
-import sys
-
-
 print("updating worker")
 
 keypath  = sys.argv[1]
 workerID = sys.argv[2]
+remoteDir = sys.argv[3]
 
-with open("geyser-plugin-config.json", "r") as jsonFile:
+with open(remoteDir+"/clockwork/lib/geyser-plugin-config.json", "r") as jsonFile:
     data = json.load(jsonFile)
 
 data["keypath"] = str(keypath)
 data["worker_id"] = int(workerID)
 
-with open("geyser-plugin-config.json", "w") as jsonFile:
+with open(remoteDir+"/clockwork/lib/geyser-plugin-config.json", "w") as jsonFile:
     json.dump(data, jsonFile, indent=4,sort_keys=True)
