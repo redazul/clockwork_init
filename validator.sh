661a9f214ed80a3926c3ea61256d42c4c94d69fa
azuldev <91290695+redazul@users.noreply.github.com>
2022-12-10 19:30:15 -0500

    Update validator.sh

--- validator.sh
+++ validator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo pkill sol
+pkill sol
 
 sudo $(command -v solana-sys-tuner) --user $(whoami) > sys-tuner.log 2>&1 &
 
-pkill sol
+
 sudo $(command -v solana-sys-tuner) --user $(whoami) > sys-tuner.log 2>&1 &
 solana-validator  --log /var/log/solana/solana_validator.log --identity /home/jc/.config/solana/id.json --rpc-port 8899 --only-known-rpc --full-rpc-api --known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 --known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ --known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ --known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S --private-rpc    --entrypoint entrypoint.mainnet-beta.solana.com:8001 --entrypoint entrypoint2.mainnet-beta.solana.com:8001 --entrypoint entrypoint3.mainnet-beta.solana.com:8001 --entrypoint entrypoint4.mainnet-beta.solana.com:8001 --entrypoint entrypoint5.mainnet-beta.solana.com:8001 --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d --wal-recovery-mode skip_any_corrupted_record --limit-ledger-size --geyser-plugin-config /home/jc/clockwork/lib/geyser-plugin-config.json &    
 
