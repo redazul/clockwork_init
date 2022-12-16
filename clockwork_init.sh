a6ad650906fd349436c573c8a29eccff70cb7394
azuldev <91290695+redazul@users.noreply.github.com>
2022-12-15 20:09:20 -0500

    Update clockwork_init.sh

--- clockwork_init.sh
+++ clockwork_init.sh
@@ -8,9 +8,10 @@ echo "[0] get path update"
 wget https://raw.githubusercontent.com/redazul/clockwork_init/main/export_path.sh 
 chmod +x export_path.sh
 
-echo "[1] create log directory"
+echo "[1] configure logging"
 sudo mkdir /var/log/solana
 sudo chmod 777 /var/log/solana
+wget https://raw.githubusercontent.com/redazul/clockwork_init/main/solana -o /etc/logrotate.d/solana
 
 echo "[2] getting latest solana build"
 sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
